8929cade330abbe26e84fd477cbd17763441ceb7
johndoeadams <johndoeadams@gmail.com>
2018-12-15 12:01:39 -0500

    update html,css and js

---
+++ assets/js/game.js
@@ -0,0 +1,25 @@
+  var Question=[
+    {
+    ask:"Why is a duck?" ,
+    answer:"Because it has flat feet"
+  },
+  {
+    ask:"What is the ultimate answer?" ,
+    answer:"42"
+  },
+  {
+    ask:"Why?" ,
+    answer:"Why not?"
+  },
+];
+
+
+//creates a new question from the list of available questions
+function generateQuestion(){
+
+}
+
+//refreshes the screen and html elements
+function updateGame(){
+
+}
